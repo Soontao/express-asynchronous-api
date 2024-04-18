daa09d8b03f5819d9d3a41b1e7ef615a20fc7ec1
Theo Sun <theo.sun@outlook.com>
2024-04-18 12:43:20 +0800

    fix: ut

--- test/index.test.mjs
+++ test/index.test.mjs
@@ -1,8 +1,9 @@
 import { equal } from "node:assert";
-import { describe, it } from "node:test";
+import { describe, it, mock } from "node:test";
 import { createAsyncApiMiddleware } from "../src/index.mjs";
 
 describe("express async simple tests", () => {
+  mock.timers.enable({ apis: ["setInterval"] });
   it("should support create new middleware", () => {
     equal(typeof createAsyncApiMiddleware({}), "function");
   });
